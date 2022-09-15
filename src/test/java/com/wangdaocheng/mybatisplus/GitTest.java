b8c03dc8e3c5cdd97f69ac0054084de2b401de69
wonder <wonder@qq.com>
2022-09-15 17:02:44 +0800

    third commit

--- src/test/java/com/wangdaocheng/mybatisplus/GitTest.java
+++ src/test/java/com/wangdaocheng/mybatisplus/GitTest.java
@@ -3,5 +3,6 @@ package com.wangdaocheng.mybatisplus;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git");
+        System.out.println("git22");
     }
 }
