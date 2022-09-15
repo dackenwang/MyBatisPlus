d7a3cacf8f866ab1d2ed5e762eafb0a61cec4fd1
wonder <wonder@qq.com>
2022-09-15 17:08:40 +0800

    hotfix commit

--- src/test/java/com/wangdaocheng/mybatisplus/GitTest.java
+++ src/test/java/com/wangdaocheng/mybatisplus/GitTest.java
@@ -4,5 +4,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git");
         System.out.println("git22");
+        System.out.println("hotfix code");
+
+
     }
 }
